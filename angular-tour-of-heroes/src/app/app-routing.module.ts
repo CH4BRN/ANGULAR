51e88b3dbc9fcc6163fc9a48dd5d72c09b18787a
pierre.antoine <pierre.antoine@CFD-L009>
2019-10-07 10:39:40 +0200

    added toutes

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
+/**
+ A typical Angular Route has two properties:
+    path: a string that matches the URL in the browser address bar.
+    component: the component that the router should create when navigating to this route.
 
+ */
+const routes: Routes =[
+  { 
+    path: 'heroes', 
+    component: HeroesComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  //  First imports RouterModule and Routes so the app can have routing functionality.
+  //  Routes tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar.
+  //  forRoot() method supplies the service providers and directives needed for routing, 
+  //  and performs the initial navigation based on the current browser URL
+  imports: [RouterModule.forRoot(routes)],
+  //  Gives the Router somewhere to go once you configure the routes.
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }

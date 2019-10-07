d6dba224d9b5c6a2d8a7adfd06e2d5df0be8600b
pierre.antoine <pierre.antoine@CFD-L009>
2019-10-07 10:12:49 +0200

    added asynchronous

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of} from 'rxjs';
 
 // Injectable marks the class to participate in the dependency injection system. 
 @Injectable({
@@ -9,4 +10,17 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
   constructor() { }
+
+  /**First getHeroes method
+  
+  getHeroes(): Hero[]{{
+    return HEROES;
+  }}
+
+  */
+ 
+  //  of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES);
+  }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+// Import the mocked heroes
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,17 +20,51 @@ export class HeroesComponent implements OnInit {
   */
 
   // property to expose the HEROES array for binding.
-  heroes = HEROES;
+  // With the mocked heroes ...
+  //  heroes = HEROES;
+
+  // With the service ...
+  heroes: Hero[];
 
   // property to get the selected hero.
   selectedHero: Hero;
 
-  constructor() { }
+  //  parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+  constructor(
+    private heroService: HeroService
+  ) { }
 
+  /**
+   * Angular lifecycle hook
+   */
   ngOnInit() {
+    this.getHeroes();
       }
 
+  /** 
+   * Called when a hero is selected. 
+   * */
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+
+  /** 
+   * Get the heroes from the service 
+   * */
+    // First getHeroes() method.
+    // Assigns an array of heroes to the component's heroes property
+    /*  
+      getHeroes(): void{
+        this.heroes = this.heroService.getHeroes();
+      }
+    */
+
+   // With Observable getHeroes() method. (= Asynchronous)
+   // waits for the Observable to emit the array of heroes—which could 
+   // happen now or several minutes from now. The subscribe() method passes
+   // the emitted array to the callback, which sets the component's heroes property.
+  getHeroes(): void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
   }
 }

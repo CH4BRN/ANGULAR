61a9b8414dcf4619dd76daaaae0a43df03ebac26
pierre.antoine <pierre.antoine@CFD-L009>
2019-10-07 11:20:05 +0200

    updated dashboard component

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  // links are styled as colored blocks by the dashboard.component.css.
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  //  defines a heroes array property.
+  heroes: Hero[] = [];
 
+  //  expects Angular to inject the HeroService into a private heroService property.
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  //  lifecycle hook calls getHeroes()
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  /**
+   * returns the sliced list of heroes at positions 1 and 5, returning only four of the Top Heroes (2nd, 3rd, 4th, and 5th).
+   */
+  getHeroes(): void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }

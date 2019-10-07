2b26d46782045e4bfc33c28121ae1da77c185589
pierre.antoine <pierre.antoine@CFD-L009>
2019-10-07 16:24:06 +0200

    error handling

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,6 +5,7 @@ import { Observable, of, from} from 'rxjs';
 // Service-in-service secnario : Message-s into hero-s into hero-c
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 // Injectable marks the class to participate in the dependency injection system. 
 @Injectable({
@@ -46,15 +47,34 @@ export class HeroService {
 
   /** Third getHeroes method :
    * - GET heroes from the server. Uses the HttpClient.
+   * 
+   * You can swap between second and thrid method because both functions return an Observable<Hero[]> ("an observable of hero arrays")
+   * HttpClient.get() returns the body of the response as an untyped JSON object by default. Applying the optional type specifier, <Hero[]> , gives you a typed result object.
+   * 
+   * catchError() operator intercepts an Observable that failed.
    */
   getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
-    
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
+  private handleError<T> (operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
 
-  /**
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+    
+   /**
    * Get hero by ID.
    * @param id 
    */
